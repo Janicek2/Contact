519f1d809d05822d3e6dcecdf1ce990fad3fbfe6
Bartosz Janicki <janicek2@gmail.com>
2018-06-18 19:39:03 +0200

    Add JSX

--- components/Contact.js
+++ components/Contact.js
@@ -5,17 +5,12 @@ var Contact = React.createClass({
   
     render: function () {
       return (
-        React.createElement('div', {className: 'contactItem'},
-          React.createElement('img', {
-            className: 'contactImage',
-            src: 'https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&h=650&w=940'
-          }),
-          React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-          React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
-          React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
-            this.props.item.email
-          )
-        )
+        <div className={"contactItem"}>
+        <img className={"contactImage"} src={"http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico"}/>
+        <p className={"contactLabel"}>Imię: {this.props.item.firstName}</p>
+        <p className={"contactLabel"}>Nazwisko: {this.props.item.lastName}</p>
+        <a className={"contactEmail"} href={"mailo:" + this.props.item.email}> {this.props.item.email}</a>
+      </div>
       );
     }
   });
