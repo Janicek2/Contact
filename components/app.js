ee2a7113b7d6bd0402377c9f34516dc8cf6409f5
Bartosz Janicki <janicek2@gmail.com>
2018-07-19 08:20:04 +0200

    Add promise

--- components/app.js
+++ components/app.js
@@ -1,3 +1,6 @@
+var GIPHY_API_URL = "http://api.giphy.com";
+var GIPHY_PUB_KEY = "iY1scEBoYDc4pIhhgmgt3lNHtLk3SWN4";
+
 App = React.createClass({
 
     getInitialState () {
@@ -7,45 +10,51 @@ App = React.createClass({
         gif: {}
       }
     },
-  
+
     handleSearch: function (searchingText) {
       this.setState({
         loading: true
       })
-  
-      this.getGif(searchingText, function (gif) {
-        this.setState({ 
-          loading: false,
-          gif: gif,
-          searchingText: searchingText
-        })
-      }.bind(this)) 
+
+      this.getGif(searchingText).then((response) => {
+
+          this.setState({
+            loading: false,
+            gif: {
+              url: response.data.data.fixed_width_downsampled_url,
+              sourceUrl: response.data.data.url
+            },
+            searchingText: searchingText
+          })
+
+      })
     },
-  
-    getGif: function(searchingText, callback) { 
+
+    getGif: function(searchingText) {
         var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.onload = function() {
-            if (xhr.status === 200) {
-               var data = JSON.parse(xhr.responseText).data;
-                var gif = {
-                    url: data.fixed_width_downsampled_url,
-                    sourceUrl: data.url
-                };
-                callback(gif);
-            }
-        };
-        xhr.send();
+        return axios.get(url);
+        // var xhr = new XMLHttpRequest();
+        // xhr.open('GET', url);
+        // xhr.onload = function() {
+        //     if (xhr.status === 200) {
+        //        var data = JSON.parse(xhr.responseText).data;
+        //         var gif = {
+        //             url: data.fixed_width_downsampled_url,
+        //             sourceUrl: data.url
+        //         };
+        //         callback(gif);
+        //     }
+        // };
+        // xhr.send();
     },
-  
+
     render: function () {
       var styles = {
         margin: '0 auto',
         textAlign: 'center',
         width: '60%'
       }
-  
+
       return (
         <div style={styles}>
           <h1>Wyszukiwarka GIFów!</h1>
@@ -62,3 +71,6 @@ App = React.createClass({
       )
     }
   })
+
+var app = React.createElement(App);
+ReactDOM.render(app, document.getElementById('app'));
